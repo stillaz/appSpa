8f86ed2959fcb9e7b04a57f64dc3b7db7ecaf492
Alexander Aguirre Zorrilla <aguirrealexorri@gmail.com>
2018-05-28 19:03:34 -0500

    Inicio del proyecto de Barberia Barbero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Calendar } from '@ionic-native/calendar';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { ReservaPage } from '../pages/reserva/reserva';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { PersonaProvider } from '../providers/persona/persona';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    ReservaPage,
     TabsPage
   ],
   imports: [
@@ -26,15 +24,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    ReservaPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Calendar,
+    PersonaProvider
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/pages/detalle-persona/detalle-persona.ts
@@ -0,0 +1,86 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, ViewController, ToastController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { PersonaProvider } from '../../providers/persona/persona';
+
+/**
+ * Generated class for the DetallePersonaPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-detalle-persona',
+  templateUrl: 'detalle-persona.html',
+})
+export class DetallePersonaPage {
+
+  todo: FormGroup;
+  nuevo: boolean = true;
+
+  constructor(public navCtrl: NavController, public viewCtrl: ViewController, private formBuilder: FormBuilder, private persona: PersonaProvider, public toastCtrl: ToastController ) {
+    this.form({});
+  }
+
+  form(datosPersona){
+    this.todo = this.formBuilder.group({
+      idpersona: [datosPersona.idpersona, Validators.required],
+      nombre: [datosPersona.nombre, Validators.required],
+      telefono: [datosPersona.telefono, Validators.required],
+      correoelectronico: [datosPersona.correoelectronico]
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetallePersonaPage');
+  }
+
+  getPersona(){
+    /*if(this.todo.value.idpersona){
+      this.persona.getById(this.todo.value.idpersona).then(res => {
+        if(res){
+          this.form(res);
+          this.nuevo = false;
+        } else{
+          this.nuevo = true;
+        }
+      }).catch(err => alert("Error cargando datos de la persona"));
+    }*/
+  }
+
+  guardar(){
+    let datosPersona = this.todo.value;
+    /*if(this.nuevo){
+      this.todo.patchValue({activo: true});
+      this.persona.create(datosPersona).then(res => {
+        let toast = this.toastCtrl.create({
+          message: 'Los datos de la persona han sido ingresadas',
+          duration: 2000,
+          position: 'top'
+        });
+        toast.present()
+        this.viewCtrl.dismiss(datosPersona);
+      }).catch(err => {
+        alert("Error creando persona");
+      });
+    } else{
+      this.persona.update(datosPersona).then(res => {
+        let toast = this.toastCtrl.create({
+          message: 'Los datos de la persona han sido modificados',
+          duration: 3000,
+          position: 'top'
+        });
+        toast.present()
+        this.viewCtrl.dismiss(datosPersona);
+      }).catch(err => {
+        alert("Error modificando datos de la persona");
+      });
+    }*/
+  }
+
+  cerrar(){
+    this.viewCtrl.dismiss({});
+  }
+
+}
